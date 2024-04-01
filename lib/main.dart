460ddf1083c1bb0fffa37e6869d7c9f13528efcf
ukitomato <me@ukitomato.dev>
2024-04-01 19:39:25 +0900

    #40

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,20 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.white,
         body: Container(),
       ),
-    ),
-  );
+    );
+  }
 }
